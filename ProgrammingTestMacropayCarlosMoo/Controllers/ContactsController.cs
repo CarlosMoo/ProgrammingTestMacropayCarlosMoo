c8c21c716ec074bd20043756e7f1a39c9a640092
Carlos Moo <LE17081431@merida.tecnm.mx>
2022-08-13 02:27:55 -0500

    Programming Test Macropay Commit

---
+++ ProgrammingTestMacropayCarlosMoo/Controllers/ContactsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammingTestMacropayCarlosMoo.Models;
+using System.Text.Json;
+
+namespace ProgrammingTestMacropayCarlosMoo.Controllers
+{
+    public class ContactsController : MacroPayDefaultController
+    {
+        private string _connectionString = @"C:\Users\Carlos Moo\source\repos\ProgrammingTestMacropayCarlosMoo\ProgrammingTestMacropayCarlosMoo\Database\fakedatabase.js";
+        #region GET /contacts
+        [HttpGet()]
+        public ActionResult<List<Contacts>> GetAll()
+        {
+            Request.ContentType = "application/json";
+            var request = HttpContext.Request.Query["phrase"];
+            if (request.ToString() == null || request.ToString().Length == 0)
+            {
+                return BadRequest();
+            }
+            string json = System.IO.File.ReadAllText(_connectionString);
+            var listContacts = JsonSerializer.Deserialize<List<Contacts>>(json).AsQueryable().OrderBy(contact => contact.name).ToList();
+            var result = listContacts.FindAll(contactresult => contactresult.name.ToUpper().Contains(request.ToString().ToUpper()));
+            return result;
+        }
+        #endregion
+
+        #region GET /contacts/
+        [HttpGet("{id}")]
+        public ActionResult<Contacts> GetById(string id)
+        {
+            string json = System.IO.File.ReadAllText(_connectionString);
+            var result = JsonSerializer.Deserialize<List<Contacts>>(json).Where(contact => contact.id == id).FirstOrDefault();
+            if(result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        #endregion
+
+        #region DELETE /contacts/
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            string json = System.IO.File.ReadAllText(_connectionString);
+            var contacts = JsonSerializer.Deserialize<List<Contacts>>(json);
+            foreach (var contact in contacts)
+            {
+                if (contact.id == id)
+                {
+                    var validate = contacts.Remove(contact);
+                    TextWriter writeJson = new StreamWriter(_connectionString);
+                    writeJson.Write(JsonSerializer.Serialize(contacts));
+                    writeJson.Flush();
+                    writeJson.Close();
+
+                    if (validate)
+                    {
+                        return NoContent();
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+            return NotFound();
+        }
+        #endregion
+    }
+}
---
+++ ProgrammingTestMacropayCarlosMoo/Models/Contacts.cs
@@ -0,0 +1,15 @@
+﻿namespace ProgrammingTestMacropayCarlosMoo.Models
+{
+    public class Contacts
+    {
+        public string id { get; set; }
+        public List<string> addressLines { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+    }
+
+    public class Address
+    {
+        public int id { get; set; }
+    }
+}
